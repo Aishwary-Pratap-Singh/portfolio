622f959551ef712f6362dbd1a89e2574cd38de8e
Aishwary-Pratap-Singh <aishwarypsingh@gmail.com>
2023-02-21 15:28:20 +0530

    added projects to home page

--- portfolio_app/__init__.py
+++ portfolio_app/__init__.py
@@ -2,9 +2,33 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+projects = [
+    {
+        "name": "Habit tracking app with Python and MongoDB",
+        "thumb": "img/habit-tracking-hero.jpg",
+        "hero": "img/habit-tracking-hero.jpg",
+        "categories": ["python", "web"],
+        "slug": "habit-tracking",
+        "prod": "https://udemy.com",
+    },
+    {
+        "name": "Personal finance tracking app with React",
+        "thumb": "img/personal-finance.jpg",
+        "hero": "img/personal-finance.jpg",
+        "categories": ["react", "javascript"],
+        "slug": "personal-finance",
+    },
+    {
+        "name": "REST API Documentation with Postman and Swagger",
+        "thumb": "img/rest-api-docs.jpg",
+        "hero": "img/rest-api-docs.jpg",
+        "categories": ["writing"],
+        "slug": "api-docs",
+    },
+]
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", projects = projects)
 
 @app.route("/about")
 def about():
